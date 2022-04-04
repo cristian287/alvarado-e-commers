8e063f18b186ea51345486902da25eaf4519c1a7
cristian287 <cristianalvarado287@gmail.com>
2022-04-04 19:11:32 -0300

    V-0.4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import Item from './componets/itemListcontainer/itemListcontainer';
 import Navbar from './componets/navbar/navbar';
+import Contador from './componets/conter/conter';
 function App() {
+  const onCart= ( cantidad) => {
+    console.log ("se agregaron " + cantidad + " de productos")
+  }
   return (
     <div className="App">
     <Navbar></Navbar>
     <Item></Item>
+    <Contador initial={0} stock={10} carrito={onCart}  ></Contador>
       <header className="App-header">
       </header>
     </div>
---
+++ src/componets/conter/conter.js
@@ -0,0 +1,37 @@
+import {useState,useEffect} from "react";
+import "./conter.css"
+
+const Contador = ({ initial, stock, carrito })=>  {
+    const [count, setCount ] = useState (initial)
+    useEffect(() => {
+        console.log ("funciona")
+    },[])
+    const Suma = () => {
+        if ( count >= stock ) {
+            console.log (" no se puede sumar, falta de stock")
+        }
+        else {
+            setCount ( count +1)
+        }
+    }
+    const Resta = () => {
+        if ( count <= 0 ) {
+            console.log ("no se esta comprando nada")
+        }
+        else {
+            setCount ( count -1)
+        }
+    }
+    return (
+        <div>
+            <div className="contador">
+                <button onClick={Resta}> - </button>
+                <p className="display"> {count} </p>
+                <button onClick={Suma}> + </button>
+            </div>
+            <button className="compra" onClick={() => carrito (count) }>  agregar al carrito  </button>
+        </div>
+    )
+
+}
+export default Contador
